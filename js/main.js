5ad808f7571bd212c6f44974535cc5b11092c6f9
Daniel Streba <danielstreba@gmail.com>
2020-09-21 19:54:45 +0200

    Add initial design and GET requests

--- js/main.js
+++ js/main.js
@@ -1,4 +1,42 @@
 "use strict";
 
-$(document).ready(function() {
+const API_URL = "https://webtechcars.herokuapp.com";
+let STATE = {
+    cars: [],
+    manufacturers: []
+};
+
+$(document).ready(function () {
+    $(document).ajaxError(function(event, jqxhr) {
+        $(".error-message").html(jqxhr.status + " " + jqxhr.statusText);
+        $(".modal").show();
+        $(".container").hide();
+    });
+
+    $("body").load("body.html", function () {
+        $("header").load("header.html");
+        $(".container").load("content.html");
+        $("footer").load("footer.html");
+
+        $(".button-reload").on("click", function () {
+            location.reload();
+        });
+
+        getCars();
+        getManufacturers();
+    });
 });
+
+function getCars() {
+    $.get(API_URL + "/api/cars", function (res) {
+        STATE.cars = res;
+        console.log(res);
+    });
+}
+
+function getManufacturers() {
+    $.get(API_URL + "/api/manufacturers", function (res) {
+        STATE.cars = res;
+        console.log(res);
+    });
+}
